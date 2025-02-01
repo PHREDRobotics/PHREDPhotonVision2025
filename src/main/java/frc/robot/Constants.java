2a75176daf653ea3f7fe4492abcbdb1ed4629090
Steven King <steveking16@outlook.com>
2025-02-01 15:47:23 -0800

    constants up to date

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,28 +15,18 @@ public class Constants {
 
     public static final double kBumperLength = 32;
 
-    public static final Translation2d kFrontLeftLocationInches = new Translation2d(15, 15);
-    public static final Translation2d kFrontRightLocationInches = new Translation2d(15, -15);
-    public static final Translation2d kBackLeftLocationInches = new Translation2d(-15, 15);
-    public static final Translation2d kBackRightLocationInches = new Translation2d(-15, -15);
+    public static final Translation2d kFrontLeftLocationInches = new Translation2d(-21, 13.5);
+    public static final Translation2d kFrontRightLocationInches = new Translation2d(3, 13.5);
+    public static final Translation2d kBackLeftLocationInches = new Translation2d(-21, -10.5);
+    public static final Translation2d kBackRightLocationInches = new Translation2d(3, -10.5);
 
-    public static final double kMaxSpeed = 6; // 3
+    public static final double kMaxSpeed = 2 * Math.floor(Math.PI); // 6
     public static final double kMaxAngularSpeed = 6;
 
     public static final double kModuleMaxAngularVelocity = kMaxAngularSpeed;
     public static final double kModuleMaxAngularAcceleration = kModuleMaxAngularVelocity * kModuleMaxAngularVelocity; // radians per second squared
   }
   public static final class SwerveConstants {
-    public static final PIDController kDrivePIDController = new PIDController(1, 0, 0); // CHANGE THIS --------------------------------------------------
-    //public static final PIDController kTurningPIDController = new PIDController(1, 0, 0);
-    //public static final ProfiledPIDController kTurningPIDController = new ProfiledPIDController(0.9, 0.6, 0.5,
-    //  new TrapezoidProfile.Constraints(
-    //    PhysicalConstants.kModuleMaxAngularVelocity, PhysicalConstants.kModuleMaxAngularAcceleration
-    //  ));
-
-    public static final SimpleMotorFeedforward kDriveFeedforward = new SimpleMotorFeedforward(1, 3);
-    public static final SimpleMotorFeedforward kTurnFeedforward = new SimpleMotorFeedforward(1, 0.5);
-
     public static final double kWheelRadius = 0.0508;
     public static final int kEncoderResolution = 4096;
 
@@ -52,8 +42,8 @@ public class Constants {
     public static final int kFrontRightTurnMotorCANId = 12;
     public static final int kBackRightTurnMotorCANId = 17;
 
-    public static final double kDrivingMotorReduction = 4;
-    public static final double kTurningMotorReduction = 4;
+    public static final double kDrivingMotorReduction = 0.4;
+    public static final double kTurningMotorReduction = 0.4;
 
     public static final Translation2d kFrontLeftLocationMeters = new Translation2d(
             Units.inchesToMeters(Constants.PhysicalConstants.kFrontLeftLocationInches.getX()),
