d3fba453a008a056719f16223f10c828e3d170f3
Steven King <steveking16@outlook.com>
2025-02-19 10:14:35 -0800

    alphbetize constant classes

--- src/main/java/frc/robot/commands/RetractLift.java
+++ src/main/java/frc/robot/commands/RetractLift.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
 public class RetractLift extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final ClimbSubsystem m_subsystem;
 
   /**
@@ -27,15 +27,17 @@ public class RetractLift extends Command {
   @Override
   public void initialize() {
     m_subsystem.retractPneumaticClimber();
-}
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
