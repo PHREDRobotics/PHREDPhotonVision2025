e34e0d92b3e1583be05cbc5c21c6ecabe1dad4f3
Steven King <steveking16@outlook.com>
2025-02-22 15:15:23 -0800

    Updated Autos. Need to actually implement them in the code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -63,10 +63,11 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    autoChooser.setDefaultOption("Bottom Coral Left", RobotContainer.AutoSwitcher.BottomCoralLeft);
+    autoChooser.setDefaultOption("Right Coral Left", RobotContainer.AutoSwitcher.BottomCoralLeft);
     autoChooser.addOption("Bottom Coral Center", RobotContainer.AutoSwitcher.BottomCoralCenter);
     autoChooser.addOption("Bottom Coral Right", RobotContainer.AutoSwitcher.BottomCoralRight);
     autoChooser.addOption("Off-the-line", RobotContainer.AutoSwitcher.OFF_THE_LINE);
+    auto
     SmartDashboard.putData("Auto mode", autoChooser);
 
 
