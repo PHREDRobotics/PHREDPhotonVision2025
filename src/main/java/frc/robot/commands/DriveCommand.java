8930476ef7982388070b156d905fdd405b561ac6
Steven King <steveking16@outlook.com>
2025-02-24 19:35:48 -0800

    Fixed DriveCommand.

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -14,76 +14,84 @@ import frc.robot.subsystems.SwerveSubsystem;
  * A command that allows the robot to be driven with a joystick.
  */
 public class DriveCommand extends Command {
-    private SwerveSubsystem swerveDrive;
-    private DoubleSupplier ySpeed;
-    private DoubleSupplier xSpeed;
-    private DoubleSupplier rot;
-    private DoubleSupplier throttle;
-    private BooleanSupplier fieldOriented;
+        private SwerveSubsystem swerveDrive;
+        private DoubleSupplier ySpeedFunc;
+        private DoubleSupplier xSpeedFunc;
+        private DoubleSupplier rotFunc;
+        private DoubleSupplier throttle;
+        private BooleanSupplier fieldOriented;
 
-    private SlewRateLimiter xLimiter;
-    private SlewRateLimiter yLimiter;
-    private SlewRateLimiter zLimiter;
+        private SlewRateLimiter xLimiter;
+        private SlewRateLimiter yLimiter;
+        private SlewRateLimiter zLimiter;
 
-    /**
-     * Creates a new DriveCommand.
-     * @param swerveDrive The swerve subsystem
-     * @param ySpeed Forward speed
-     * @param xSpeed Sideways speed
-     * @param rot Rotational speed
-     * @param throttle Speed control
-     * @param fieldOriented Whether the robot should drive oriented to itself or the field
-     */
-    public DriveCommand(
-            SwerveSubsystem swerveDrive,
-            DoubleSupplier ySpeed,
-            DoubleSupplier xSpeed,
-            DoubleSupplier rot,
-            DoubleSupplier throttle,
-            BooleanSupplier fieldOriented) {
-        this.swerveDrive = swerveDrive;
-        this.ySpeed = ySpeed;
-        this.xSpeed = xSpeed;
-        this.rot = rot;
-        this.throttle = throttle;
-        this.fieldOriented = fieldOriented;
+        private double ySpeed;
+        private double xSpeed;
+        private double rot;
 
-        xLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAcceleration);
-        yLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAcceleration);
-        zLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAngularAcceleration);
+        /**
+         * Creates a new DriveCommand.
+         * 
+         * @param swerveDrive   The swerve subsystem
+         * @param ySpeedFunc    Forward speed
+         * @param xSpeedFunc    Sideways speed
+         * @param rotFunc       Rotational speed
+         * @param throttle      Speed control
+         * @param fieldOriented Whether the robot should drive oriented to itself or the
+         *                      field
+         */
+        public DriveCommand(
+                        SwerveSubsystem swerveDrive,
+                        DoubleSupplier ySpeedFunc,
+                        DoubleSupplier xSpeedFunc,
+                        DoubleSupplier rotFunc,
+                        DoubleSupplier throttle,
+                        BooleanSupplier fieldOriented) {
+                this.swerveDrive = swerveDrive;
+                this.ySpeedFunc = ySpeedFunc;
+                this.xSpeedFunc = xSpeedFunc;
+                this.rotFunc = rotFunc;
+                this.throttle = throttle;
+                this.fieldOriented = fieldOriented;
 
-        addRequirements(swerveDrive);
-    }
+                xLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAcceleration);
+                yLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAcceleration);
+                zLimiter = new SlewRateLimiter(Constants.PhysicalConstants.kMaxAngularAcceleration);
 
-    @Override
-    public void execute() {
-        double adjustedThrottle = ((-(Constants.ControllerConstants.kMaxThrottle
-                - Constants.ControllerConstants.kMinThrottle) / 2) * throttle.getAsDouble())
-                + (Constants.ControllerConstants.kMinThrottle + Constants.ControllerConstants.kMaxThrottle) / 2;
+                addRequirements(swerveDrive);
+        }
 
-        ySpeed = () -> MathUtil.applyDeadband(ySpeed.getAsDouble(),
-                Constants.ControllerConstants.kFlightStickYDeadband);
-        xSpeed = () -> MathUtil.applyDeadband(-xSpeed.getAsDouble(),
-                Constants.ControllerConstants.kFlightStickXDeadband);
-        rot = () -> MathUtil.applyDeadband(-rot.getAsDouble(), Constants.ControllerConstants.kFlightStickZDeadband);
+        @Override
+        public void execute() {
+                double adjustedThrottle = ((-(Constants.ControllerConstants.kMaxThrottle
+                                - Constants.ControllerConstants.kMinThrottle) / 2) * throttle.getAsDouble())
+                                + (Constants.ControllerConstants.kMinThrottle
+                                                + Constants.ControllerConstants.kMaxThrottle) / 2;
 
-        ySpeed = () -> ySpeed.getAsDouble() * Math.abs(ySpeed.getAsDouble());
-        xSpeed = () -> xSpeed.getAsDouble() * Math.abs(xSpeed.getAsDouble());
-        rot = () -> rot.getAsDouble() * Math.abs(rot.getAsDouble());
+                ySpeed = MathUtil.applyDeadband(ySpeedFunc.getAsDouble(),
+                                Constants.ControllerConstants.kFlightStickYDeadband);
+                xSpeed = MathUtil.applyDeadband(-xSpeedFunc.getAsDouble(),
+                                Constants.ControllerConstants.kFlightStickXDeadband);
+                rot = MathUtil.applyDeadband(-rotFunc.getAsDouble(),
+                                Constants.ControllerConstants.kFlightStickZDeadband);
 
-        ySpeed = () -> yLimiter.calculate(ySpeed.getAsDouble()) * adjustedThrottle;
-        xSpeed = () -> xLimiter.calculate(xSpeed.getAsDouble()) * adjustedThrottle;
-        rot = () -> zLimiter.calculate(rot.getAsDouble());
+                ySpeed = ySpeed * Math.abs(ySpeed);
+                xSpeed = xSpeed * Math.abs(xSpeed);
+                rot = rot * Math.abs(rot);
 
-        swerveDrive.drive(
-                ySpeed,
-                xSpeed,
-                rot,
-                fieldOriented);
+                ySpeed = yLimiter.calculate(ySpeed) * adjustedThrottle;
+                xSpeed = xLimiter.calculate(xSpeed) * adjustedThrottle;
+                rot = zLimiter.calculate(rot);
 
-        SmartDashboard.putNumber("Joystick/Y", ySpeed.getAsDouble());
-        SmartDashboard.putNumber("Joystick/X", xSpeed.getAsDouble());
-        SmartDashboard.putNumber("Joystick/Z", rot.getAsDouble());
-        SmartDashboard.putNumber("Joystick/T", adjustedThrottle);
-    }
+                swerveDrive.drive(
+                                () -> ySpeed,
+                                () -> xSpeed,
+                                () -> rot,
+                                fieldOriented);
+
+                SmartDashboard.putNumber("Joystick/Y", ySpeed);
+                SmartDashboard.putNumber("Joystick/X", xSpeed);
+                SmartDashboard.putNumber("Joystick/Z", rot);
+                // SmartDashboard.putNumber("Joystick/T", adjustedThrottle);
+        }
 }
