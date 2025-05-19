cd12e4d7fd11289deb6a4f6d5f2e2c4e1c0d1985
Andrew Glover <andrew@hillsideghost.com>
2025-05-18 17:01:00 -0700

    Added and fixed errors on template vision sim code from the photonlib docs

--- src/main/java/frc/robot/subsystems/vision/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/PhotonVisionSubsystem.java
@@ -8,31 +8,31 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 
-import frc.robot.Constants.PhotonVisionConstants;
+import frc.robot.Constants.VisionConstants;
 
 public class PhotonVisionSubsystem {
-    PhotonCamera camera;
-    PhotonPipelineResult result;
-    PhotonTrackedTarget bestTarget;
-    Transform3d cameraToTarget;
-    ProfiledPIDController pidX;
-    ProfiledPIDController pidY;
-    ProfiledPIDController pidZ;
-    ProfiledPIDController pidYaw;
+  PhotonCamera camera;
+  PhotonPipelineResult result;
+  PhotonTrackedTarget bestTarget;
+  Transform3d cameraToTarget;
+  ProfiledPIDController pidX;
+  ProfiledPIDController pidY;
+  ProfiledPIDController pidZ;
+  ProfiledPIDController pidYaw;
 
-    private PhotonVisionSubsystem() {
-        camera = new PhotonCamera("ArducamOV8972 1");
+  private PhotonVisionSubsystem() {
+    camera = new PhotonCamera(VisionConstants.kCameraName);
 
-    }
+  }
 
-    public void update() {
-        result = camera.getLatestResult();
-        bestTarget = result.getBestTarget();
-        cameraToTarget = bestTarget.getBestCameraToTarget();
-    }
+  public void update() {
+    result = camera.getLatestResult();
+    bestTarget = result.getBestTarget();
+    cameraToTarget = bestTarget.getBestCameraToTarget();
+  }
 
-    public Pose3d getRobotToTarget() {
-        Pose3d robotToTarget = new Pose3d().plus(PhotonVisionConstants.robotToCamera1.plus(cameraToTarget));
-        return robotToTarget;
-    }
+  public Pose3d getRobotToTarget() {
+    Pose3d robotToTarget = new Pose3d().plus(VisionConstants.kRobotToCamera1.plus(cameraToTarget));
+    return robotToTarget;
+  }
 }
