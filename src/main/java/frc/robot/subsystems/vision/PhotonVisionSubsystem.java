e15f46e6630d2df226d29725baf926457b6e3b46
Steven King <steveking16@outlook.com>
2025-07-12 14:48:48 -0700

    🚌, uh did a lot of theoretical stuff here. Added PIDs, targetCommand, globalPose code, constants, and comments.

--- src/main/java/frc/robot/subsystems/vision/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/PhotonVisionSubsystem.java
@@ -1,38 +1,116 @@
 package frc.robot.subsystems.vision;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.proto.Photon;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-
+import edu.wpi.first.math.geometry.Translation3d;
+import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.Constants.PhotonVisionConstants;
 
 public class PhotonVisionSubsystem {
+
     PhotonCamera camera;
     PhotonPipelineResult result;
     PhotonTrackedTarget bestTarget;
+    PhotonPoseEstimator photonPoseEstimator;
+
+    Transform3d robotToCamera;
     Transform3d cameraToTarget;
-    ProfiledPIDController pidX;
-    ProfiledPIDController pidY;
-    ProfiledPIDController pidZ;
-    ProfiledPIDController pidYaw;
+    Transform3d robotToTarget;
+    Pose3d prevRobotPose;
+    Pose3d currentRobotPose;
 
-    private PhotonVisionSubsystem() {
+    public ProfiledPIDController pidX;
+    public ProfiledPIDController pidY;
+    public ProfiledPIDController pidRot;
+
+    AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+    // IDK WHATS GOING ON WHY ARE THERE SO MANY TRANSFORM3D, cuz there has to be
+    // Also this code is prob bad atm, but I think its needed somewhere
+    public PhotonVisionSubsystem() {
+        // Initialize PIDs
+        pidX = new ProfiledPIDController(
+                PhotonVisionConstants.kXYP, PhotonVisionConstants.kXYI, PhotonVisionConstants.kXYD,
+                Constants.PhotonVisionConstants.kXYControllerConstraints);
+        pidY = new ProfiledPIDController(
+                PhotonVisionConstants.kXYP, PhotonVisionConstants.kXYI, PhotonVisionConstants.kXYD,
+                Constants.PhotonVisionConstants.kXYControllerConstraints);
+        pidRot = new ProfiledPIDController(
+                PhotonVisionConstants.kRP, PhotonVisionConstants.kRI, PhotonVisionConstants.kRD,
+                Constants.PhotonVisionConstants.kRControllerConstraints);
+
         camera = new PhotonCamera("ArducamOV8972 1");
+        // camera1
+        robotToCamera = PhotonVisionConstants.robotToCamera1;
 
+        // Construct PhotonPoseEstimator
+        photonPoseEstimator = new PhotonPoseEstimator(
+                Constants.PhotonVisionConstants.aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
+                robotToCamera);
+    }
+
+    // Calculates the xSpeed based off of the targets position relative to the robot
+    public double getXspeed() {
+        if (result.hasTargets()) {
+            return pidX.calculate(0, robotToTarget.getX());
+        } else {
+            return 0;
+        }
+    }
+
+    // Calculates the ySpeed based off of the targets position relative to the robot
+    public double getYspeed() {
+        if (result.hasTargets()) {
+            return pidY.calculate(0, robotToTarget.getY());
+        } else {
+            return 0;
+        }
+    }
+
+    // Calculates the rotation speed based off of the targets rotation relative to
+    // the robot
+    public double getRotSpeed() {
+        if (result.hasTargets()) {
+            return pidRot.calculate(0, robotToTarget.getRotation().getAngle());
+        } else {
+            return 0;
+        }
+    }
+
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose3d prevEstimatedRobotPose,
+            PhotonPipelineResult result) {
+        photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+        return photonPoseEstimator.update(result);
     }
 
     public void update() {
+        // Sets the prevPose to the currentPose
+        prevRobotPose = currentRobotPose;
         result = camera.getLatestResult();
-        bestTarget = result.getBestTarget();
-        cameraToTarget = bestTarget.getBestCameraToTarget();
-    }
-
-    public Pose3d getRobotToTarget() {
-        Pose3d robotToTarget = new Pose3d().plus(PhotonVisionConstants.robotToCamera1.plus(cameraToTarget));
-        return robotToTarget;
+        // Checks if the camera has a target, then does target related operations
+        if (result.hasTargets() == true) {
+            bestTarget = result.getBestTarget();
+            cameraToTarget = bestTarget.getBestCameraToTarget();
+            robotToTarget = robotToCamera.plus(cameraToTarget);
+        }
+        // Sets the currentPose using the prevPose
+        // gives global pos, ignore for now
+        currentRobotPose = getEstimatedGlobalPose(prevRobotPose, result).orElse(null).estimatedPose;
     }
 }
